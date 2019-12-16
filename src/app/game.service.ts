a55670501aa8fc94f608f355af041615516e8dc4
Martin <martin.z.atanasov@gmail.com>
2019-12-16 16:24:18 +0200

    center-corner-corner strategy working properly

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -35,12 +35,14 @@ export class GameService {
     [3, 2, 6],
     [7, 4, 8],
     [9, 8, 6]
-  ]
+  ];
   winingCombinations = [
     [1, 2, 3],
-    [3, 6, 9],
+    [4, 5, 6],
     [7, 8, 9],
     [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
     [1, 5, 9],
     [3, 5, 7]
   ];
@@ -79,9 +81,13 @@ export class GameService {
     }
     if (game.turn === 5) {
       const isMiddle = this.middles.includes(game.playerTurns[1]);
-      if (isMiddle) {
+      if (isMiddle && game.strategy === 'center-corner') {
         this.gameStatus.next( {...game, strategy: 'center-corner-middle'});
         this.centerCornerMidTurn();
+      }
+      if (!isMiddle && game.strategy === 'center-corner') {
+        this.gameStatus.next( {...game, strategy: 'center-corner-corner'});
+        this.centerCornerCornerTurn();
       }
     }
     switch (game.strategy) {
@@ -94,7 +100,43 @@ export class GameService {
       case 'center-corner-middle':
         this.centerCornerMidTurn();
         break;
+      case 'center-corner-corner':
+        this.centerCornerCornerTurn();
+        break;
       default: break;
+    }
+  }
+
+  centerCornerCornerTurn() {
+    const game = this.gameStatus.getValue();
+    if (game.turn === 5) {
+      const winMove = this.getWiningMark(this.winingCombinations, game.playerTurns);
+      this.markBox(winMove, 'x', true);
+    }
+    if (game.turn === 7) {
+      const winCombinations = this.filterWinCombinations(game.playerTurns);
+      const winMove = this.getWiningMark(winCombinations, game.computerTurns);
+      // If a win move mark it, else mark random empty box
+      if (winMove) {
+        this.markBox(winMove, 'x', true);
+        console.log('COMPUTER WINS');
+      } else {
+        const emptyBox = this.randomEmptyBox(true);
+        this.markBox(emptyBox, 'x', true);
+      }
+    }
+    if (game.turn === 9) {
+      const winCombinations = this.filterWinCombinations(game.playerTurns);
+      const winMove = this.getWiningMark(winCombinations, game.computerTurns);
+      // If a win move mark it, else mark random empty box
+      if (winMove) {
+        this.markBox(winMove, 'x', true);
+        console.log('COMPUTER WINS');
+      } else {
+        const emptyBox = this.randomEmptyBox();
+        this.markBox(emptyBox, 'x', true);
+        console.log('DRAW');
+      }
     }
   }
 
@@ -102,19 +144,12 @@ export class GameService {
     const game = this.gameStatus.getValue();
     if (game.turn === 5) {
       const playerWinMove = this.getWiningMark(this.winingCombinations, game.playerTurns);
-      if (playerWinMove) {
-        this.markBox(playerWinMove, 'x', true);
-      } else {
-        // if player will not score
-      }
+      this.markBox(playerWinMove, 'x', true);
     }
-
     if (game.turn === 7) {
-      // Need to filter the array from player turns
-      let winingCombinations = [...this.winingCombinations];
-      const winMove = this.getWiningMark(this.winingCombinations, game.computerTurns);
-      console.log(winMove);
-      // this.markBox(winMove, 'x', true);
+      const winingCombinations = this.filterWinCombinations(game.playerTurns);
+      const winMove = this.getWiningMark(winingCombinations, game.computerTurns);
+      this.markBox(winMove, 'x', true);
     }
   }
 
@@ -151,7 +186,7 @@ export class GameService {
       ];
       const secondMove = game.computerTurns[1];
       const getRightCombination = winingCombinations.filter( v => v.includes(secondMove))[0];
-      let winMove = getRightCombination.filter( v => v !== secondMove)[0];
+      const winMove = getRightCombination.filter( v => v !== secondMove)[0];
       if (!game.playerTurns.includes(winMove)) {
         this.markBox(winMove, 'x', true);
       } else {
@@ -182,7 +217,7 @@ export class GameService {
       winCombinations = winCombinations.filter( v =>
         !(v.includes(game.playerTurns[0]) || v.includes(game.playerTurns[1]) || v.includes(game.playerTurns[2]))
       );
-      let winMove = this.getWiningMark(winCombinations, game.computerTurns);
+      const winMove = this.getWiningMark(winCombinations, game.computerTurns);
       this.markBox(winMove, 'x', true);
     }
   }
@@ -191,10 +226,11 @@ export class GameService {
     turns = turns.sort();
     let matches = 0;
     let winComb = null;
-    for (let i=0; i < winCombinations.length; i++) {
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < winCombinations.length; i++) {
       winCombinations[i].forEach( value => {
         turns.includes(value) ? matches++ : matches += 0;
-      })
+    });
       if (matches === 2) {
         winComb = winCombinations[i];
         break;
@@ -203,7 +239,26 @@ export class GameService {
         winComb = null;
       }
     }
-    let winMove = winComb ? winComb.filter( v => !turns.includes(v))[0] : 0;
+    const winMove = winComb ? winComb.filter( v => !turns.includes(v))[0] : 0;
     return winMove;
+  }
+
+  filterWinCombinations(turns: number[]): number[][] {
+    const game = this.gameStatus.getValue();
+    let winingCombinations = [...this.winingCombinations];
+    turns.forEach( v => {
+      winingCombinations = winingCombinations.filter( value => !value.includes(v));
+    });
+    return winingCombinations;
+  }
+
+  randomEmptyBox(isMiddle?: boolean): number {
+    const game = this.gameStatus.getValue();
+    let boxes = game.boardBoxes.filter( v => !v.markType);
+    if (isMiddle) {
+      boxes = boxes.filter( v => this.middles.includes(v.id));
+    }
+    const emptyBox = boxes[Math.floor(Math.random() * boxes.length)];
+    return emptyBox.id;
   }
 }
